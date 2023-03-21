f37c56635da13a8225335cb9e6377b47da8ae996
Milton Vento <mv9@williams.edu>
2023-03-21 02:53:05 -0400

    final commit

--- react/barista-app/src/App.jsx
+++ react/barista-app/src/App.jsx
@@ -3,8 +3,7 @@ import './App.css';
 import BaristaForm from './Components/baristaForm';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <div>
       <div className='title-container'>

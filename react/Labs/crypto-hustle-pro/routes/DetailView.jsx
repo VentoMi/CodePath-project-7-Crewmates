1d9bd21db762dcca1436270aa906ba512506adf7
Milton Vento <mv9@williams.edu>
2023-04-06 23:17:42 -0400

    new gif

---
+++ react/Labs/crypto-hustle-pro/routes/DetailView.jsx
@@ -0,0 +1,12 @@
+import CoinDetails from "./CoinDetail"
+
+
+const DetailView = () => {
+    return (
+        <div>
+            <CoinDetails />
+        </div>
+    )
+}
+
+export default DefaultView;
